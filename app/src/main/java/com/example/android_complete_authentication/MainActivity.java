11271178663e65b8d5922688a4a95e72e11c2702
masum184e <masum184e@gmail.com>
2024-09-11 21:44:14 +0600

    initial commit

---
+++ app/src/main/java/com/example/android_complete_authentication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android_complete_authentication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
