dce239f211ec2c27657b0bb6ac755527af4719bb
masum184e <masum184e@gmail.com>
2024-09-13 04:53:37 +0600

    validation added

--- app/src/main/java/com/example/android_complete_authentication/Login.java
+++ app/src/main/java/com/example/android_complete_authentication/Login.java
@@ -9,17 +9,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Login extends AppCompatActivity {
     private TextView signupLink;
     private Button btnSignIn;
+    private TextInputLayout getEmail, getPassword;
+    private TextInputEditText emailEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signupLink=findViewById(R.id.signupLink);
-        btnSignIn=findViewById(R.id.btnSignIn);
+        signupLink = findViewById(R.id.signupLink);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        getEmail = findViewById(R.id.getEmail);
+        getPassword = findViewById(R.id.getPassword);
+
+        emailEditText = (TextInputEditText)getEmail.getEditText();
+        passwordEditText = (TextInputEditText)getPassword.getEditText();
 
         signupLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,11 +42,21 @@ public class Login extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mainIntent = new Intent(Login.this, Profile.class);
-                startActivity(mainIntent);
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    getEmail.setError("Email is required");
+                } else {
+                    getEmail.setError(null);
+                }
+
+                if (password.isEmpty()) {
+                    getPassword.setError("Password is required");
+                } else {
+                    getPassword.setError(null);
+                }
             }
         });
-
     }
-
 }
