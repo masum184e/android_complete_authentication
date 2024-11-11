f566b5577a2e6be5558c657b3e1abe1c800b283b
masum184e <masum184e@gmail.com>
2024-11-11 13:11:15 +0600

    Retrofit conf added

---
+++ app/src/main/java/com/example/android_complete_authentication/api/RetrofitClient.java
@@ -0,0 +1,18 @@
+package com.example.android_complete_authentication.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit = null;
+
+    public static ApiService getApiService() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://10.0.2.2:80/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiService.class);
+    }
+}
