fead59f398483c6eb702fe41db56e6ee28bc55c2
masum184e <masum184e@gmail.com>
2024-11-12 09:59:42 +0600

    profile backend integrated

--- app/src/main/java/com/example/android_complete_authentication/Profile.java
+++ app/src/main/java/com/example/android_complete_authentication/Profile.java
@@ -1,45 +1,54 @@
 package com.example.android_complete_authentication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.android_complete_authentication.fragments.ProfileFragment;
 import com.example.android_complete_authentication.fragments.SettingFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import com.google.android.material.navigation.NavigationBarView;
-
 public class Profile extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigation;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // IF USER IS NOT LOGIN REDIRECT TO LOGIN ACTIVITY
+        sharedPreferences = getSharedPreferences("AUTHENTICATION", Context.MODE_PRIVATE);
+        String bearerToken = sharedPreferences.getString("authToken", null);
+
+        if (bearerToken == null) {
+            unauthorizedUser();
+            return;
+        }
+
+        // SETUP ACTIVITY LAYOUT
         setContentView(R.layout.activity_profile);
 
-        bottomNavigation=findViewById(R.id.bottomNavigation);
-        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int currentItemId=menuItem.getItemId();
-
-                if(currentItemId==R.id.navigation_profile)load_fragment(new ProfileFragment());
-                else load_fragment(new SettingFragment());
-
-                return true;
-            }
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            loadFragment(item.getItemId() == R.id.navigation_profile ? new ProfileFragment() : new SettingFragment());
+            return true;
         });
+
         bottomNavigation.setSelectedItemId(R.id.navigation_profile);
     }
-    public void load_fragment(Fragment fragment){
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.profile_layout_container, fragment).commit();
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.profile_layout_container, fragment).commit();
     }
 
+    private void unauthorizedUser() {
+        Toast.makeText(this, "Unauthorized User", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(this, Login.class));
+        finish();
+    }
 }
--- app/src/main/java/com/example/android_complete_authentication/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/android_complete_authentication/fragments/ProfileFragment.java
@@ -17,7 +17,6 @@ import android.widget.Toast;
 
 import com.example.android_complete_authentication.Login;
 import com.example.android_complete_authentication.R;
-import com.example.android_complete_authentication.Registration;
 import com.example.android_complete_authentication.api.ApiService;
 import com.example.android_complete_authentication.api.RetrofitClient;
 import com.example.android_complete_authentication.models.ProfileModel;
@@ -28,65 +27,63 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ProfileFragment extends Fragment {
+
     private TextView userName, userEmail;
     private ImageView userProfilePicture;
     private SharedPreferences sharedPreferences;
-    private String BEARER_TOKEN;
-    private Button logoutBtn;
+    private String bearerToken;
+    private Button logoutBtn, editBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // SELECTING ELEMENT
         userName = view.findViewById(R.id.userName);
         userEmail = view.findViewById(R.id.userEmail);
+        userProfilePicture = view.findViewById(R.id.userProfilePicture);
         logoutBtn = view.findViewById(R.id.logoutBtn);
+        editBtn = view.findViewById(R.id.editBtn);
         sharedPreferences = requireActivity().getSharedPreferences("AUTHENTICATION", Context.MODE_PRIVATE);
 
-        logoutBtn.setOnClickListener(btnView -> {
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.remove("authToken");
-            editor.commit();
-            Toast.makeText(getContext(),"Logged Out Successfully",Toast.LENGTH_LONG).show();
-            startActivity(new Intent(getContext(), Login.class));
-        });
+        // HANDLING LOG OUT
+        logoutBtn.setOnClickListener(v -> logout());
 
-
-        if(sharedPreferences.contains("authToken")){
-            BEARER_TOKEN = sharedPreferences.getString("authToken", null);
-        }
-
-        if (BEARER_TOKEN != null) {
-            fetchUserDetails(BEARER_TOKEN);
+        // CHECKING TOKEN
+        bearerToken = sharedPreferences.getString("authToken", null);
+        if ((bearerToken == null)) {
+            unauthorizedUser();
         } else {
-            Toast.makeText(getContext(), "Unauthorized User", Toast.LENGTH_LONG).show();
+            fetchUserDetails();
         }
 
         return view;
     }
-    private void fetchUserDetails(String BEARER_TOKEN) {
+
+    private void logout() {
+        sharedPreferences.edit().remove("authToken").apply();
+        Toast.makeText(getContext(), "Logged Out Successfully", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(getContext(), Login.class));
+        requireActivity().finish();
+    }
+
+    private void unauthorizedUser() {
+        Toast.makeText(getContext(), "Unauthorized User", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(getContext(), Login.class));
+        requireActivity().finish();
+    }
+
+    private void fetchUserDetails() {
         ApiService apiService = RetrofitClient.getApiService();
-        Call<ProfileResponse> call = apiService.getUserProfile("Bearer " + BEARER_TOKEN);
+        Call<ProfileResponse> call = apiService.getUserProfile("Bearer " + bearerToken);
 
         call.enqueue(new Callback<ProfileResponse>() {
             @Override
             public void onResponse(Call<ProfileResponse> call, Response<ProfileResponse> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    ProfileResponse profileResponse = response.body();
-                    Toast.makeText(getContext(), profileResponse.getMessage(), Toast.LENGTH_LONG).show();
-
-                    if ("true".equals(profileResponse.isStatus())) {
-                        ProfileModel profile = profileResponse.getProfile();
-
-                        userName.setText(profile.getFullname());
-                        userEmail.setText(profile.getEmail());
-
-                        System.out.println("HELLO WORLD");
-                        System.out.println(profile.getFullname());
-                        System.out.println(profile.getEmail());
-
-                    } else {
-                        Toast.makeText(getContext(), "Failed to fetch data", Toast.LENGTH_LONG).show();
-                    }
+                if (response.isSuccessful() && response.body() != null && "true".equals(response.body().isStatus())) {
+                    ProfileModel profile = response.body().getProfile();
+                    userName.setText(profile.getFullname());
+                    userEmail.setText(profile.getEmail());
                 } else {
                     Toast.makeText(getContext(), "Failed to fetch data", Toast.LENGTH_LONG).show();
                 }
@@ -98,6 +95,4 @@ public class ProfileFragment extends Fragment {
             }
         });
     }
-
-
 }
