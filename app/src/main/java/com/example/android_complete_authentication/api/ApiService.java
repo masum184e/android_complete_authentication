2f85c9ccffa0d9522b15a55508f2e10bbe9a5202
masum184e <masum184e@gmail.com>
2024-11-12 08:35:20 +0600

    registration route added

--- app/src/main/java/com/example/android_complete_authentication/api/ApiService.java
+++ app/src/main/java/com/example/android_complete_authentication/api/ApiService.java
@@ -3,6 +3,8 @@ package com.example.android_complete_authentication.api;
 import com.example.android_complete_authentication.models.LoginRequest;
 import com.example.android_complete_authentication.models.LoginResponse;
 import com.example.android_complete_authentication.models.ProfileResponse;
+import com.example.android_complete_authentication.models.RegistrationRequest;
+import com.example.android_complete_authentication.models.RegistrationResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -17,4 +19,6 @@ public interface ApiService {
     @GET("php_complete_authentication/user/profile")
     Call<ProfileResponse> getUserProfile(@Header("Authorization") String bearerToken);
 
+    @POST("php_complete_authentication/user/registration")
+    Call<RegistrationResponse> registrationUser(@Body RegistrationRequest registrationRequest);
 }
