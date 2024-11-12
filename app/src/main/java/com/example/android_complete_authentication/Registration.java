f3de630e8fb9b18c4cb7d563074ea3c064c8a3fa
masum184e <masum184e@gmail.com>
2024-11-12 09:34:11 +0600

    registration api integrated

--- app/src/main/java/com/example/android_complete_authentication/Registration.java
+++ app/src/main/java/com/example/android_complete_authentication/Registration.java
@@ -6,11 +6,21 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.android_complete_authentication.api.ApiService;
+import com.example.android_complete_authentication.api.RetrofitClient;
+import com.example.android_complete_authentication.models.RegistrationRequest;
+import com.example.android_complete_authentication.models.RegistrationResponse;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Registration extends AppCompatActivity {
 
@@ -60,11 +70,34 @@ public class Registration extends AppCompatActivity {
             getEmail.setError(email.isEmpty() ? "Email is required" : null);
             getPassword.setError(password.isEmpty() ? "Password is required" : null);
 
-            if (!email.isEmpty() && !password.isEmpty() && !name.isEmpty()) registerUser(email, password, name);
+            if (!email.isEmpty() && !password.isEmpty() && !name.isEmpty()) registerUser(name, email, password);
         });
     }
     private void registerUser(String name, String email, String password) {
-        System.out.println(name+" "+email+" "+password);
-        startActivity(new Intent(this, Profile.class));
+        ApiService apiService = RetrofitClient.getApiService();
+        Call<RegistrationResponse> call = apiService.registrationUser(new RegistrationRequest(name, email, password));
+
+        call.enqueue(new Callback<RegistrationResponse>() {
+            @Override
+            public void onResponse(Call<RegistrationResponse> call, Response<RegistrationResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    RegistrationResponse registrationResponse = response.body();
+                    Toast.makeText(Registration.this, registrationResponse.getMessage(), Toast.LENGTH_LONG).show();
+                    if ("true".equals(registrationResponse.isStatus())) {
+                        getSharedPreferences("AUTHENTICATION", Context.MODE_PRIVATE).edit().putString("authToken", registrationResponse.getToken()).apply();
+                        startActivity(new Intent(Registration.this, Profile.class));
+                        finish();
+                    }
+                } else {
+                    Toast.makeText(Registration.this, "Failed to Registration", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RegistrationResponse> call, Throwable t) {
+                Toast.makeText(Registration.this, "Network error occurred", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
